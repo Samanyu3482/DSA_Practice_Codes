0aefd213802442771a1487135a00ca607dc374f5
Samanyu 3482 <gautamsamanyu3482@gmail.com>
2026-07-27 13:12:54 +0530

    CionChange

--- dp/UnboundedKnapVariations/CoinChange.java
+++ dp/UnboundedKnapVariations/CoinChange.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class CoinChange{
-    public int coinChangeWays(int[] coins, int sum){
+    public static int coinChangeWays(int[] coins, int sum){
         int n = coins.length;
         // step 1
         int dp[][] = new int [n + 1][sum + 1];
@@ -16,14 +16,21 @@ public class CoinChange{
         // step 3
         for(int i = 1; i < n + 1; i++){
             for(int j = 1; j < sum + 1; j++){
-                
+                if(coins[i - 1] <= j){
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
+
+        return dp[n][sum];
 
 
     }
     public static void main(String[] args) {
         int[] coins = {2, 5, 6, 3};
         int sum = 10;
+        System.out.println(coinChangeWays(coins, sum));
     }
 }
