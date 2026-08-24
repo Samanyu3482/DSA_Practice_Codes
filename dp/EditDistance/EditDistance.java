adcaffc8aa8113a1facba8c91588c46f8e6d779f
Samanyu 3482 <gautamsamanyu3482@gmail.com>
2026-08-24 11:58:25 +0530

    Edit Distance

---
+++ dp/EditDistance/EditDistance.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class EditDistance {
+    public static int editDistance(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for(int i = 0; i < n + 1; i++) {
+            for(int j = 0; j < m + 1; j++) {
+                if(i == 0) {
+                    dp[i][j] = j;
+                }else if(j == 0) {
+                    dp[i][j] = i;
+                }else{
+                    continue;
+                }
+            }
+        }
+
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 1; j < m + 1; j++) {
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int ans1 = dp[i - 1][j] + 1;
+                    int ans2 = dp[i - 1][j - 1] + 1;
+                    int ans3 = dp[i][j - 1] + 1;
+                    int min = Math.min(Math.min(ans1, ans2), ans3);
+                    dp[i][j] = min;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+    public static void main(String[] args) {
+        String word1 = "intention";
+        String word2 = "execution";
+        System.out.println(editDistance(word1, word2));
+    }
+}
